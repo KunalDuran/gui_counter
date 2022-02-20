36d68c0040a979e2e483be92c9180c469c5aa7df
Kunal Duran <kunalduran13@gmail.com>
2022-02-20 16:37:22 +0530

    version2

--- gui_counter.py
+++ gui_counter.py
@@ -1,10 +1,22 @@
 import tkinter as tk
 from tkinter import *
 import time, json, os, datetime
+from tkinter import ttk
+from tkcalendar import *
+
 
 CONFIG = 'config.json'
 clock_running = False
 paused = True
+if not os.path.exists(CONFIG):
+    with open(CONFIG, 'w') as f:
+        data = {}
+        data['time'] = 0
+        data['already_studied'] = 0
+        data['target_date'] = 0
+        json.dump(data, f)
+
+
 
 
 def start():
@@ -59,22 +71,15 @@ def reset():
     global clock_running, paused
     try: text.configure(text=f'Time Spent : 00:00:00')
     except: pass
-    with open(CONFIG, 'w') as f:
-        data = {}
+    with open(CONFIG, 'r+') as f:
+        data = json.load(f)
         data['time'] = 0
         data['already_studied'] = 0
+        f.seek(0)
+        f.truncate(0)
         json.dump(data, f)
     clock_running = False
     paused = False
-
-def remaining_days():
-    target_date = datetime.datetime(2021,6,27)
-    today = datetime.datetime.utcnow()
-    return (target_date - today).days
-
-
-if not os.path.exists(CONFIG):
-    reset()
 
 
 window = tk.Tk()
@@ -99,59 +104,114 @@ window.configure(background='#ffffff')
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=1)
 
-header = tk.Label(window, text="Time Tracker", width=45, height=2, fg="white", bg="#333333",
+tabControl = ttk.Notebook(window)
+
+tab1 = ttk.Frame(tabControl)
+tab2 = ttk.Frame(tabControl)
+tab3 = ttk.Frame(tabControl)
+
+tabControl.add(tab1, text='Time Tracker')
+tabControl.add(tab2, text='Goal Timeout')
+tabControl.add(tab3, text='Streak Counter')
+
+tabControl.pack(expand=1, fill="both")
+
+# header = tk.Label(window, text="Time Tracker", width=45, height=1, fg="white", bg="#333333",
+#                     font=('times', 18, 'bold', 'underline'))
+# header.place(x=0, y=20)
+
+
+
+
+
+
+
+
+
+
+############################  TAB 1 CONTENT ##################################
+
+#### TIME TRACKER BAR
+study_time = json.load(open(CONFIG))['already_studied']
+study_time = str(datetime.timedelta(seconds=round(study_time)))
+text = tk.Label(tab1, text=f"Time Spent : {study_time}", width=45, height=2, fg="white", bg="#333333",
                     font=('times', 18, 'bold', 'underline'))
-header.place(x=0, y=0)
+text.place(x=0, y=50)
 
-
-# study_time = json.load(open(CONFIG))['already_studied']
-# study_time = str(datetime.timedelta(seconds=round(study_time)))
-text = tk.Label(window, text=f"Time Spent : 00:00:00", width=45, height=2, fg="white", bg="#333333",
-                    font=('times', 18, 'bold', 'underline'))
-text.place(x=0, y=90)
-
-'''
-def display_time(study_time):
-    study_time = str(datetime.timedelta(seconds=round(study_time)))
-    text = tk.Label(window, text=f"Time Spent {study_time}", width=45, height=2, fg="white", bg="#333333",
-                        font=('times', 18, 'bold', 'underline'))
-    text.place(x=0, y=90)
-
-display_time(study_time)'''
-
-
-
-
-
-
-var = StringVar()
-var.set(str(remaining_days()))
-dayLeft = tk.Label(window, text=f"Days Remaining : {var.get()}", width=45, height=2, fg="white", bg="#333333",
-                    font=('times', 18, 'bold', 'underline'))
-dayLeft.place(x=0, y=180)
-
-
-start_btn = tk.Button(window, text="Start", command=start, fg="white", bg="#525252", width=15,
+#### START BUTTON
+start_btn = tk.Button(tab1, text="Start", command=start, fg="white", bg="#525252", width=15,
                     height=2,
                     activebackground="#118ce1", font=('times', 15, ' bold '))
-start_btn.place(x=10, y=350)
+start_btn.place(x=10, y=275)
 
-pause_btn = tk.Button(window, text="Pause", command=pause, fg="white", bg="#525252", width=15,
+#### PAUSE BUTTON
+pause_btn = tk.Button(tab1, text="Pause", command=pause, fg="white", bg="#525252", width=15,
                         height=2,
                         activebackground="#118ce1", font=('times', 15, ' bold '))
-pause_btn.place(x=205, y=350)
+pause_btn.place(x=205, y=275)
 
-reset_btn = tk.Button(window, text="Reset", command=reset, fg="white", bg="#525252",
+#### RESET BUTTON
+reset_btn = tk.Button(tab1, text="Reset", command=reset, fg="white", bg="#525252",
                         width=15,
                         height=2,
                         activebackground="#118ce1", font=('times', 15, ' bold '))
-reset_btn.place(x=400, y=350)
+reset_btn.place(x=400, y=275)
 
 
+
+
+
+############################  TAB 2 CONTENT ##################################
+def remaining_days(target_date=None):
+    # target_date = datetime.datetime(2021,6,27)
+    today = datetime.datetime.utcnow()
+    if not target_date: target_date = today
+    return (target_date - today).days
+
+
+
+def calender_pop():
+    today = datetime.datetime.utcnow()
+    cal = Calendar(tab2, selectmode='day',year=today.year, month=today.month, day=today.day)
+    cal.pack(pady=90)
+    
+    def get_date():
+        target_date = datetime.datetime.strptime(cal.get_date() , '%m/%d/%y')
+        with open(CONFIG, 'r+') as f:
+            data = json.load(f)
+            data['target_date'] = remaining_days(target_date)
+            f.seek(0)
+            f.truncate(0)
+            json.dump(data, f)
+
+        cal.destroy()
+        cal_btn.destroy()
+        dayLeft.config(text=f"Days Remaining is : {remaining_days(target_date)}")
+    cal_btn = tk.Button(tab2, text='Select', command=get_date)
+    cal_btn.place(x=275, y=275)
+
+
+dayLeft = tk.Label(tab2, text=f"Days Remaining : {json.load(open(CONFIG))['target_date']}", width=45, height=2, fg="white", bg="#333333",
+                    font=('times', 18, 'bold', 'underline'))
+dayLeft.place(x=0, y=30)
+
+
+pause_btn = tk.Button(tab2, text="Set Date", command=calender_pop, fg="white", bg="#525252", width=15,
+                        height=2,
+                        activebackground="#118ce1", font=('times', 15, ' bold '))
+pause_btn.place(x=205, y=300)
+
+
+
+
+
+############################  TAB 3 CONTENT ##################################
+
+
+
+############################  FOOTER CONTENT ##################################
 link2 = tk.Label(window, text="Copyright©2020, kunalduran.com", fg="blue", font=('times',8) )
 link2.place(x=430, y=430)
-
-
 def site(link):
     import webbrowser
     webbrowser.open(link)
@@ -164,13 +224,13 @@ def update_timer():
     # text.after(1000, update_day_count)
 
 
-def update_day_count():
-    dayLeft.configure(text=f"Days Remaining : {str(remaining_days())}")
-    dayLeft.after(1000, update_day_count)
+# def update_day_count():
+#     dayLeft.configure(text=f"Days Remaining : {str(remaining_days())}")
+#     dayLeft.after(1000, update_day_count)
 
 
 
 
-update_day_count()
+# update_day_count()
 
 window.mainloop()
